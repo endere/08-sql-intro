b68d5908df0d6690f9ec55cb17dc701d598bfdbb
Scott Brenden <scott.b.brenden@gmail.com>
2017-03-24 09:36:05 -0700

    Finished final NOTEs

--- starter-code/server.js
+++ starter-code/server.js
@@ -147,7 +147,7 @@ app.listen(PORT, function() {
 
 //////// ** DATABASE LOADER ** ////////
 ////////////////////////////////////////
-// NOTE:This function sends a 
+// NOTE:This function sends a query request to the server for a count of all articles. The database will find the number of articles. Then for each Article in the data file hackerIpsum.json it will parse the JSON into objects and the inserts them into a table. This uses CRUD 'read', and 'update'. that goes through 2-4
 function loadArticles() {
   client.query('SELECT COUNT(*) FROM articles')
   .then(result => {
@@ -167,7 +167,7 @@ function loadArticles() {
   })
 }
 
-// NOTE:
+// NOTE: loadDB sends a querey request to the server to create a table 'articles' if there is one does not exist. It gives the table a colum for easch of the elements in the article objects (these are typed in, it doesn't know anything about what articles are at this point). If the table is created without error then loadArticles() will be run on the new table. If there is an error it will log the error to the console. This is CRUD 'create' that runs through 2-4 (potentially 5 if there is an error).
 function loadDB() {
   client.query(`
     CREATE TABLE IF NOT EXISTS articles (
